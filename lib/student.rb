9e94afa8567885e1cacdc8d8157773928137943a
Scott  Keplinger <skeplin17@gmail.com>
2020-05-20 17:30:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,17 @@
 class Student < User
 
 attr_accessor :knowledge
+
   def initialize
     @knowledge = []
   end
 
   def learn(string)
     @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
   end
 
 
